ffb8e7b896924452e5db791f137335d956704937
michaely <wei.yin@bluechips.net.cn>
2015-12-09 13:46:18 +0800

    add Carthage

--- Gesp/Gesp/AppDelegate.swift
+++ Gesp/Gesp/AppDelegate.swift
@@ -16,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow()
+        self.window!.rootViewController = UIViewController()
+        
+        let mainview = self.window!.rootViewController!.view
+        let v = UIView(frame: CGRectMake(100,100,50,50))
+        v.backgroundColor = UIColor.blueColor()
+        mainview.addSubview(v)
+        self.window!.backgroundColor = UIColor.brownColor()
+        self.window!.makeKeyAndVisible()
         return true
     }
 
